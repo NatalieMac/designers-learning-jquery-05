5dcb1959dddced92114f41f65bb8d2d9a4691fc9
Natalie MacLees <natalie@nataliemac.com>
2014-09-08 19:35:55 -0700

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,23 @@
+// $('#albi').addClass('self').next().addClass('selected');
+
+// $('#albi').addClass('self').nextAll().addClass('selected');
+
+// $('#sena').addClass('self').prev().addClass('selected');
+
+// $('#sena').addClass('self').prevAll().addClass('selected');
+
+// $('#albi').addClass('self').parent().addClass('selected');
+
+// $('#albi').addClass('self').parents().addClass('selected');
+
+// $('#albi').addClass('self').parents('ul').addClass('selected');
+
+// $('#hedy').addClass('self').children().addClass('selected');
+
+// $('#hedy').addClass('self').find('*').addClass('selected');
+
+// $('#albi').addClass('self').siblings().addClass('selected');
+
+// $('#albi').addClass('self').siblings().andSelf().addClass('selected');
+
+$('#sena').addClass('self').parents('.family').find('.species').first().find('li').addClass('selected');
